71ab0ac208cb17449d35d3494427c59b22492be3
Simone Colombo <simone.colombo@elmec.it>
2024-03-14 17:09:19 +0100

    some reordering of keymaps

--- nvimc/init.lua
+++ nvimc/init.lua
@@ -80,10 +80,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 -- Buffer keymaps
 -- format buffer
--- vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format(), { desc = '[B]uffer [F]ormat' })
+vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format, { desc = '[B]uffer [F]ormat' })
 -- save buffer
 vim.keymap.set('n', '<leader>bs', '<cmd>w<CR>', { desc = '[B]uffer [W]rite' })
 vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = '[B]uffer [W]rite' })
+vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = '[B]buffer [D]elete' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -91,7 +92,7 @@ vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = '[B]uffer [W]rite' })
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -101,7 +102,6 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<leader>|', '<cmd>vsplit<CR>', { desc = 'Split window vertically' })
 vim.keymap.set('n', '<leader>-', '<cmd>hsplit<CR>', { desc = 'Split window horizontally' })
@@ -113,9 +113,6 @@ vim.keymap.set('n', '<C-right>', '<cmd>vertical resize +5<CR>', { desc = 'Move f
 vim.keymap.set('n', '<C-left>', '<cmd>vertical resize -5<CR>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-up>', '<cmd>horizontal resize +3<CR>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-down>', '<cmd>horizontal resize -3<CR>', { desc = 'Move focus to the lower window' })
-
--- buffers
-vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = '[B]buffer [D]elete' })
 
 --[[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
