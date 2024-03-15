4d217ad6cf548b1f8c750452393cb359be06635a
Simone Colombo <simone.colombo@elmec.it>
2024-03-15 21:22:02 +0100

    substituted leap with flash

--- nvimc/lua/custom/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'ggandor/leap.nvim',
-  keys = {
-    { 's', '<Plug>(leap-forward)', {'n', 'x', 'o'}, desc = 'Leap forward' },
-    { 'S', '<Plug>(leap-backward)', {'n', 'x', 'o'}, desc = 'Leap backwards' },
-    -- { 'gs', '<Plug>(leap-from-window)', {'n', 'x', 'o'}, desc = 'Leap in other window' },
-  },
-  opts = {},
-}
