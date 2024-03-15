e8b5897e25b5e1262957b88104670f20c960985d
Simone Colombo <simone.colombo@elmec.it>
2024-03-15 21:21:51 +0100

    incorporated all comment plugins in one file. added ts-context-comment plugin

--- nvimc/lua/custom/plugins/comment.lua
+++ nvimc/lua/custom/plugins/comment.lua
@@ -1,1 +1,5 @@
-return { 'numToStr/Comment.nvim', opts = {} }
+return {
+  { 'numToStr/Comment.nvim', opts = {} },
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
