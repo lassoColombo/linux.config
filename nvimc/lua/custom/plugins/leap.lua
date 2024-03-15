13ebaf85f8d050853518b3100f7598325c6cc58b
Simone Colombo <simone.colombo@elmec.it>
2024-03-15 17:05:33 +0100

    added leap

---
+++ nvimc/lua/custom/plugins/leap.lua
@@ -0,0 +1,9 @@
+return {
+  'ggandor/leap.nvim',
+  keys = {
+    { 's', '<Plug>(leap-forward)', {'n', 'x', 'o'}, desc = 'Leap forward' },
+    { 'S', '<Plug>(leap-backward)', {'n', 'x', 'o'}, desc = 'Leap backwards' },
+    -- { 'gs', '<Plug>(leap-from-window)', {'n', 'x', 'o'}, desc = 'Leap in other window' },
+  },
+  opts = {},
+}
