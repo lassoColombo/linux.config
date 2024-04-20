6eeb588b88eedcbe287f99b3953d08ebb9d1e199
Simone Colombo <simone.colombo@elmec.it>
2024-04-20 14:27:20 +0200

    hoping copy paste works :)

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -3,9 +3,9 @@
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', "<cmd>execute 'nohlsearch | NoiceDismiss'<CR>")
-vim.keymap.set('n', '<leader>p', '"*p', {desc='[P]aste from system clipboard'})
-vim.keymap.set('n', '<leader>y', '"*y',{desc='[Y]ank to system clipboard'})
+vim.keymap.set({ 'n', 'v' }, '<Esc>', "<cmd>execute 'nohlsearch | NoiceDismiss'<CR>")
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"*p', { desc = '[P]aste from system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"*y', { desc = '[Y]ank to system clipboard' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', function()
