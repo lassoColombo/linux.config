c1f0ce7b1f8db4ba84684badac36f0492d3824c8
Simone Colombo <simone.colombo@elmec.it>
2024-04-29 16:05:20 +0200

    class doc done

--- nvim/lua/custom/treesitter/node-actions/python.lua
+++ nvim/lua/custom/treesitter/node-actions/python.lua
@@ -8,12 +8,19 @@ return {
     require 'custom.treesitter.node-actions.pythonacts.f-string',
   },
   ['list'] = {
-    require 'custom.treesitter.node-actions.pythonacts.list',
+    require 'custom.treesitter.node-actions.pythonacts.list.toggle-multilineness',
   },
   ['dictionary'] = {
     require 'custom.treesitter.node-actions.pythonacts.dict.literaldict-to-dictconstructor',
+    require 'custom.treesitter.node-actions.pythonacts.dict.toggle-multilineness',
   },
   ['identifier'] = {
     require 'custom.treesitter.node-actions.pythonacts.dict.dictconstructor-to-literaldict',
+  },
+  ['class_definition'] = {
+    require 'custom.treesitter.node-actions.pythonacts.doc.class',
+  },
+  ['function_definition'] = {
+    require 'custom.treesitter.node-actions.pythonacts.doc.method',
   },
 }
--- nvim/lua/custom/treesitter/node-actions/pythonacts/doc/class.lua
+++ nvim/lua/custom/treesitter/node-actions/pythonacts/doc/class.lua
@@ -1,49 +1,50 @@
 local helpers = require 'ts-node-action.helpers'
 
-local function get_init(class_body)
-  for body_child in class_body:iter_children() do
-    if body_child:type() == 'function_definition' then
-      local function_name = class_body:next_named_sibling()
-      if helpers.node_text(function_name) == '__init__' then
-        return body_child
-      end
-    end
-  end
-  return nil
-end
-
 return {
   function(node)
     local query = vim.treesitter.query.parse(
       'python',
       [[
-        (function_definition
-          (identifier) @qwe (#eq? @qwe "__init__")
-        )
-    ]]
+          (
+            (class_definition
+              name: (identifier) @classname
+              superclasses: (argument_list) @superclasses
+              body: (block) @body)
+          )
+        ]]
     )
-    local pt = {}
+    local clname, superclasses, body = nil, nil, nil
     for _, matches, _ in query:iter_matches(node, 0) do
+      if #matches ~= 3 then
+        print '😥 something went wrong'
+        return
+      end
+      clname = matches[1]
+      superclasses = matches[2]
+      body = matches[3]
     end
 
-    local original_text = helpers.node_text(node)
+    local docstring = { '\t"""', string.format('\t%s ...', helpers.node_text(clname)) }
 
-    local class_name = nil
-    local class_body = nil
-    for child in node:iter_children() do
-      local ntype = child:type()
-      if ntype == 'identifier' then
-        class_name = child
-      elseif ntype == 'block' then
-        class_body = child
+    if superclasses then
+      table.insert(docstring, '\tIt inherits from:')
+      for i = 0, superclasses:named_child_count() - 1 do
+        local class = helpers.node_text(superclasses:named_child(i))
+        table.insert(docstring, string.format('\t\t- %s: ...', class))
       end
     end
-    if not (class_body and class_name) then
-      vim.notify('something went wrong', vim.log.levels.WARN)
-      return original_text
+    table.insert(docstring, '\t"""')
+    table.insert(docstring, '')
+
+    -- :NOTE: this is here to mock the target node.
+    -- if i were to give a real target node, it would be overridden
+    local fake_node = {}
+    fake_node.range = function()
+      local start_row, start_col, _, _ = body:range()
+      return start_row - 1, start_col - 4, start_row - 1, start_col - 4
     end
 
-    local init_method = get_init(class_body)
+    return docstring, { target = fake_node }
   end,
 
   name = 'generate doc for class',
